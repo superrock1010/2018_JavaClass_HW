ca83ac84cc1f17581ce4b096caf16859fc9b8500
USER <USER@DESKTOP-GS26550>
2018-10-01 00:09:50 +0800

    g1

---
+++ G1/test/src/org/class1/Homework.java
@@ -0,0 +1,15 @@
+package org.class1;
+
+public class Homework {
+
+	public static void main(String[] args) {
+		int a=30;
+		int b=6;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+
+	}
+
+}
